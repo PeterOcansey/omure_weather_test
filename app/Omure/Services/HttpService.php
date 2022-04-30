1f700c454c113684a41e6ccc019e34f2147e5e6f
peter <ocanseypeter@gmail.com>
2022-04-30 14:15:57 +0000

    Update HttpService to fetch openWeatherGeoData

--- app/Omure/Services/HttpService.php
+++ app/Omure/Services/HttpService.php
@@ -9,12 +9,28 @@ class HttpService
     
     public static function fetchOpenWeatherData(float $lat, float $lon)
     {
-        $response = Http::acceptJson()->get(env("OPEN_WEATHER_API", ""), [
+        $response = Http::acceptJson()->get(env("OPEN_WEATHER_API_URL", ""), [
             'lat' => $lat,
             'lon' => $lon,
             'appId' => env("OPEN_WEATHER_API_KEY", "")
         ]);
 
-        return json_decode($this->response->getBody()->getContents());
+        return json_decode($response->getBody()->getContents());
+    }
+
+
+    /**
+     * 
+     * @param String $location_name
+     * 
+     */
+    public static function fetchOpenWeatherGeoData(String $location_name)
+    {
+        $response = Http::acceptJson()->get(env("OPEN_GEO_API_URL", ""), [
+            'q' => $location_name,
+            'appId' => env("OPEN_WEATHER_API_KEY", "")
+        ]);
+
+        return json_decode($response->getBody()->getContents());
     }
 }
